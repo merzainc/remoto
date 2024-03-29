094ec7bcc7b103a1f8dae1cfa2357858a7b3b983
merzainc <h200649z@hit.ac.zw>
2024-03-29 14:33:44 +0200

    Change dashboard font to Inter

--- dashboard/src/app/(www)/layout.tsx
+++ dashboard/src/app/(www)/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from 'next';
+import '@/styles/fonts.css';
 import '@/styles/main.css';
+
 import NavBar from '@/components/navbar';
 
 export const metadata: Metadata = {
@@ -13,11 +15,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html
-      lang='en_UK'
-      className='antialiased font-sans'
-      suppressHydrationWarning={true}
-    >
+    <html lang='en_UK' className='antialiased' suppressHydrationWarning={true}>
       <body className='text-slate-500 bg-white' suppressHydrationWarning={true}>
         <div className='isolate'>
           <NavBar />
