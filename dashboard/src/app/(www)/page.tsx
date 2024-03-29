f503bfba8377df6b27805910be374db3ef5b88d5
merzainc <h200649z@hit.ac.zw>
2024-03-29 20:06:22 +0200

    Refactor: upgrade auth logic to use app router

--- dashboard/src/app/(www)/page.tsx
+++ dashboard/src/app/(www)/page.tsx
@@ -1,6 +1,14 @@
+import { authOptions, getCurrentUser } from '@/helpers';
+import { redirect } from 'next/navigation';
 import React from 'react';
 
-const IndexPage = () => {
+const IndexPage = async () => {
+  const user = await getCurrentUser();
+  console.log(user);
+  if (!user) {
+    redirect(authOptions?.pages?.signIn || '/login');
+  }
+
   return (
     <div className='mt-8 px-4'>
       <h1 className='prose text-2xl text-slate-900'>Heading 1</h1>
