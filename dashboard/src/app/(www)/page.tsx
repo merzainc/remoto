921a452144e54a996525ea5b79ba8bc00a379c8c
merzainc <h200649z@hit.ac.zw>
2024-03-29 21:26:48 +0200

    Add @remoto/ui components

--- dashboard/src/app/(www)/page.tsx
+++ dashboard/src/app/(www)/page.tsx
@@ -1,10 +1,10 @@
+import { Button } from '@/components/ui/button';
 import { authOptions, getCurrentUser } from '@/helpers';
 import { redirect } from 'next/navigation';
 import React from 'react';
 
 const IndexPage = async () => {
   const user = await getCurrentUser();
-  console.log(user);
   if (!user) {
     redirect(authOptions?.pages?.signIn || '/login');
   }
@@ -12,10 +12,7 @@ const IndexPage = async () => {
   return (
     <div className='mt-8 px-4'>
       <h1 className='prose text-2xl text-slate-900'>Heading 1</h1>
-      <p className='mt-4'>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi,
-        officiis numquam. Ipsa quo ipsum libero?
-      </p>
+      <Button className='mt-4'>Dropdown</Button>
     </div>
   );
 };
--- dashboard/src/components/navbar.tsx
+++ dashboard/src/components/navbar.tsx
@@ -1,7 +1,19 @@
 'use client';
 
+import { useSession } from 'next-auth/react';
+import { Avatar } from './ui/avatar';
+
 export default function NavBar() {
+  const { status, data: session } = useSession();
   return (
-    <div className='stick top-0 z-10 flex items-center justify-between border-b border-zinc-950/10 h-15'></div>
+    <div className='stick top-0 z-10 flex h-15 items-center justify-between border-b border-zinc-950/10 px-4'>
+      <div className=''></div>
+      <div className=''>
+        <Avatar
+          className='size-8'
+          src={status === 'loading' ? '' : session?.user?.image}
+        />
+      </div>
+    </div>
   );
 }
